e01c1112b67f4502a830e15ff984cf37fde9f120
Moonsyn <msy9427@naver.com>
2019-10-12 21:15:55 +0900

    splash Activity 추가

--- app/src/main/java/com/example/ddh/SplashActivity.java
+++ app/src/main/java/com/example/ddh/SplashActivity.java
@@ -2,20 +2,28 @@ package com.example.ddh;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 
 public class SplashActivity extends AppCompatActivity {
 
+    private int SPLASH_DISPLAY_TIME = 1500;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
-        finish();
-
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                /* 스플래시 액티비티를 스택에서 제거. */
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_TIME);
     }
+
 }
